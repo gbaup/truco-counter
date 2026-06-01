9aab3491e4c1324a383b5b233dd6715ecdb409cd
gastonbauer <gastonbauerp@gmail.com>
2026-06-01 14:15:40 -0300

    feat: disable group fetching on public routes in useMyGroups hook

--- hooks/useMyGroups.ts
+++ hooks/useMyGroups.ts
@@ -1,9 +1,18 @@
+"use client";
+
 import { useQuery } from "@tanstack/react-query";
+import { usePathname } from "next/navigation";
 import { getMyGroups } from "@/services/groupService";
 
+const PUBLIC_ROUTES = ["/login", "/register", "/join"];
+
 export const useMyGroups = () => {
+  const pathname = usePathname();
+  const isPublicRoute = PUBLIC_ROUTES.some((p) => pathname?.startsWith(p));
+
   return useQuery({
     queryKey: ["groups", "me"],
     queryFn: getMyGroups,
+    enabled: !isPublicRoute,
   });
 };
